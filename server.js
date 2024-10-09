f228d31bb3a8a4abf8ca38cf58972eb700b15693
Aryan-Shrivastav9891 <65902181+Aryan-Shrivastav9891@users.noreply.github.com>
2024-10-09 08:37:38 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,74 @@
-const express = require("express")
-const {MongoClient , ServerApiVersion} = require("mongodb")
-const EmployeeRoute = require("./routes/addRouter.js")
-const env = require("dotenv").config()
-const app = express()
-const port = process.env.PORT || 8082
+const express = require("express");
+const app = express();
+const path = require("path");
+require("dotenv").config();
+const ejsLayout = require("express-ejs-layouts");
 
-console.log(port);
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const MongoUrl = process.env.url;
 
-app.get("/" , (req ,res)=>{
-    res.send("sucessfully")
-})
+app.set("view engine", "ejs");
+app.use(ejsLayout);
+app.set("layout", "layout/common");
+app.use(express.urlencoded({ extended: true }));
 
-app.use ("/api/employee" , EmployeeRoute)
+let NewStd , database
 
-app.listen(port , ()=>{
-    console.log(`server connnected ${port}`);
-})
+const Client = new MongoClient(MongoUrl, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const connectDB = async () => {
+  try {
+    await Client.connect();
+    console.log("mongoDb connected true✅");
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectDB();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/List", (req, res) => {
+  const data = NewStd.find().toArray()
+  res.render("listData" , {data});
+});
+
+//! add data
+app.get("/add", (req, res) => {
+  res.render("addData");
+});
+
+app.post("/add", async (req, res) => {
+  const Student = {
+    name: req.body.name,
+    age: parseInt(req.body.age),
+    email: req.body.email,
+  };
+  console.log(Student);
+  // res.render("submit");
+
+  try {
+    database = Client.db("NewStudent");
+    NewStd = database.collection("NewStd");
+    const result = await NewStd.insertOne(Student);
+    console.log(result);
+    res.render("submit", { student: Student });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
